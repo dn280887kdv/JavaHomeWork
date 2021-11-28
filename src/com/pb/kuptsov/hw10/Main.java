461b8244a490ee203c73e14647709cc9ab53f170
Feri1987 <joinmee@mail.ru>
2021-11-29 00:08:10 +0200

    hw10 (вывод на экран результатов)

--- src/com/pb/kuptsov/hw10/Main.java
+++ src/com/pb/kuptsov/hw10/Main.java
@@ -3,6 +3,23 @@ package com.pb.kuptsov.hw10;
 public class Main {
     public static void main(String[] args) {
         NumBox<Float> numBox = new NumBox<>(3);
+        numBox.setMas(0, 2.7F);
+        numBox.setMas(1, 2.2F);
+        numBox.setMas(2, 2.1F);
+
         System.out.println(numBox.max());
+        System.out.println(numBox.average());
+        System.out.println(numBox.sum());
+
+
+
+        NumBox<Integer> integerNumBox = new NumBox<>(3);
+        integerNumBox.setMas(0, 3);
+        integerNumBox.setMas(1, 6);
+        integerNumBox.setMas(2, 7);
+
+        System.out.println(integerNumBox.max());
+        System.out.println(integerNumBox.average());
+        System.out.println(integerNumBox.sum());
     }
 }
